e9facc56b4688d7fd13d749753b5e7359322e86f
Sajid <dsajid942@gmail.com>
2024-02-07 15:58:10 +0530

    dynamic urls

--- messenger/urls.py
+++ messenger/urls.py
@@ -3,5 +3,6 @@ from messenger import views
 
 urlpatterns=[
     path('',views.home,name="homeScreen"),
-    path('<str:name>',views.home2,name="dynamic")
+    path('<str:name>',views.home2,name="dynamic"),
+    path('<str:movie>/<str:hero>/<int:year>',views.home3,name="soNso")
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -11,3 +11,6 @@ def home2(request,name):
     marks=[23,52,56,73]
     context={'Hero':name,'age':40,'sequence':marks}
     return render(request,'index.html',context)
+
+def home3(request,movie,hero,year):
+    return render(request,'movie.html',{'movie_name':movie,'released':year,'hero':hero})
